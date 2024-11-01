426e7cfd3586642e6ed445e57dd77b2c79e0dac8
Spenda4life <michaelhanselL@gmail.com>
2024-11-01 17:06:19 -0400

    doesn't currently run, need to fix error

--- app.py
+++ app.py
@@ -1,49 +1,46 @@
 from flask import Flask, render_template, redirect, url_for, request
 from google.cloud import storage
 import random
-import json
+import networkx as nx
+import pickle
 
-def write_json_to_gcs(file_name, data):
+
+def write_graph_to_gcs(file_name, graph):
     blob = bucket.blob(file_name)
-    json_data = json.dumps(data)
-    blob.upload_from_string(json_data, content_type='application/json')
+    data = pickle.dumps(graph)
+    blob.upload_from_string(data=data)
     print(f"Data written to gs://{bucket_name}/{file_name}")
 
-def read_json_from_gcs(file_name):
+
+def read_graph_from_gcs(file_name):
     blob = bucket.blob(file_name)
-    json_data = blob.download_as_text()
-    data = json.loads(json_data)
-    return data
+    data = blob.download_as_string()
+    graph = pickle.loads(data)
+    return graph
 
-def load_json(path):
-   with open(path, 'r', encoding="utf-8") as f:
-        data = json.load(f)
-   return data
 
-def update_edges(options, selection):
-    '''Update the edges list based on options and user selection'''
-    # load edges
-    edges = read_json_from_gcs('edges.json')
+def update_graph(options,selection):
+    '''Update network graph based on user selection'''
+    G = read_graph_from_gcs('graph.pickle')
 
-    # loop for each possible edge
-    for i in range(4):
-        for j in range(i + 1, 4):
-
-            # get edge index from two node IDs
-            index = (
-                int(options[i]) * (len(nodes) - 1) 
-                - (int(options[i]) * (int(options[i]) + 1) // 2) 
-                + (int(options[j]) - int(options[i]) - 1)
-            )
-
-            # update edge "distance value" based on selection
+    num_opts = len(options)
+    for i in range(num_opts):
+        for j in range(i + 1, num_opts):
             if options[i] in selection and options[j] in selection:
-                edges[index] -= 1 # gets closer
-            else:
-                edges[index] += 1 # gets farther 
+                # get node id for both options and add edge between them
+                node1 = get_node(G, "commander", options[i])
+                node2 = get_node(G, "commander", options[j])
+                G.add_edge(node1, node2)
 
-    # save edges
-    write_json_to_gcs('edges.json', edges)
+    # write_graph_to_gcs('graph.pickle', G)
+    print(f'Adding edge between {node1} and {node2}')
+
+
+def get_node(G, attr_name, attr_value):
+    '''Return graph node with the matching attribute'''
+    for node, attr in G.nodes(data=True):
+        if attr.get(attr_name) == attr_value:
+            return node
 
 
 app = Flask(__name__)
@@ -53,17 +50,18 @@ bucket_name = 'ptero_cloud_storage'
 client = storage.Client()
 bucket = client.bucket(bucket_name)
 
-nodes = load_json('data/nodes.json')
+G = read_graph_from_gcs('graph.pickle')
 
 @app.route('/')
 def home():
-    return render_template('index.html', decks=random.sample(nodes, 4))
+    decks = [G.nodes[node] for node in random.sample(list(G.nodes), 4)]
+    return render_template('index.html', decks=decks)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     options = request.form.getlist('options')
     selection = request.form.getlist('selection')
-    update_edges(options,selection)
+    update_graph(options,selection)
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
